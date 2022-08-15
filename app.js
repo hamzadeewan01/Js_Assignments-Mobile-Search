4305deae5c18fe92c7681e05e64c5fad15eaa581
Muhammad Hamza Deewan <102538357+hamzadeewan01@users.noreply.github.com>
2022-08-15 08:55:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,588 @@
+var mobilePhones = {
+    // iphone
+    iphone: {
+        iphone6: {
+            name: "Iphone 6",
+            brand: "Apple",
+            colors: ["red", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-6-4.jpg",
+            isPta: false,
+        },
+
+
+        iphone6s: {
+            name: "Iphone 6s",
+            brand: "Apple",
+            colors: ["pink", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-6s1.jpg",
+            isPta: false,
+        },
+        iphone6Plus: {
+            name: "Iphone 6 plus",
+            brand: "Apple",
+            colors: ["gray", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-6-plus2.jpg",
+            isPta: false,
+        },
+        iphone6sPlus: {
+            name: "Iphone 6s plus",
+            brand: "Apple",
+            colors: ["pink", "white"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-6s-plus.jpg",
+            isPta: false,
+        },
+        iphone13: {
+            name: "Iphone 13",
+            brand: "Apple",
+            colors: ["blue", "bluewhite"],
+            image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13.jpg",
+            isPta: false,
+        },
+    },
+    // samsung
+    samsung: {
+        A235G: {
+            name: "Samsung Galaxy A23 5G",
+            brand: "Samsung",
+            colors: ["pink", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a23-5g.jpg",
+            isPta: true,
+        },
+        M135G: {
+            name: "Samsung Galaxy M13 5G",
+            brand: "Samsung",
+            colors: ["white", "Brown"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m13-5g.jpg",
+            isPta: true,
+        },
+        M13: {
+            name: "Samsung Galaxy M13",
+            brand: "Samsung",
+            colors: ["blue", "bluewhite"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m13-4g-india.jpg",
+            isPta: true,
+        },
+        A13: {
+            name: "Samsung Galaxy A13 (SM-A137)",
+            brand: "Samsung",
+            colors: ["white", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a13-a137.jpg",
+            isPta: true,
+        },
+        Xcover6: {
+            name: "Samsung Galaxy Xcover 6",
+            brand: "Samsung",
+            colors: ["black", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-xcover6-pro-r.jpg",
+            isPta: true,
+        },
+    },
+    // Vivo
+    vivo: {
+        iqoo: {
+            name: "Vivo iQOO 10 Pro",
+            brand: "Vivo",
+            colors: ["black", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/vivo-iqoo10-pro.jpg",
+            isPta: true,
+        },
+        iqoo9T: {
+            name: "Vivo iQOO 9T",
+            brand: "Vivo",
+            colors: ["white", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/vivo-iqoo-9t-5g.jpg",
+            isPta: true,
+        },
+        X80: {
+            name: "Vivo X80 Pro",
+            brand: "Vivo",
+            colors: ["blue", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/vivo-x80-pro.jpg",
+            isPta: true,
+        },
+        y21: {
+            name: "Vivo Y21",
+            brand: "Vivo",
+            colors: ["white", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/vivo-y21-.jpg",
+            isPta: true,
+        },
+        v23: {
+            name: "Vivo V23 5G",
+            brand: "vivo",
+            colors: ["orange", "orange"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/vivo-v23-5g.jpg",
+            isPta: true,
+        },
+    },
+    // Realme
+    realme: {
+        c35: {
+            name: "Realme C35",
+            brand: "Realme",
+            colors: ["green", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/realme-c35.jpg",
+            isPta: true,
+        },
+        pro9: {
+            name: "Realme 9 Pro",
+            brand: "Realme",
+            colors: ["green", "full-green"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/realme-9-pro.jpg",
+            isPta: true,
+        },
+        Gt2: {
+            name: "Realme GT2 Explorer Master",
+            brand: "Realme",
+            colors: ["orange", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/realme-gt2-master-explorer.jpg",
+            isPta: true,
+        },
+        proplus9: {
+            name: "Realme 9 Pro+",
+            brand: "Realme",
+            colors: ["white", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/realme-9-pro-plus.jpg",
+            isPta: true,
+        },
+        realme9: {
+            name: "Realme 9",
+            brand: "Realme",
+            colors: ["orange", "orange"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/realme-9-4g.jpg",
+            isPta: true,
+        },
+    },
+    // oppo
+    oppo: {
+        reno8: {
+            name: "Oppo Reno8",
+            brand: "Oppo",
+            colors: ["orange", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/oppo-reno8.jpg",
+            isPta: true,
+        },
+        reno8pro: {
+            name: "Oppo Reno8 Pro",
+            brand: "Oppo",
+            colors: ["blue", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/oppo-reno8-pro.jpg",
+            isPta: true,
+        },
+        a3s: {
+            name: "Oppo A3s",
+            brand: "Oppo",
+            colors: ["red", "red"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/oppo-a3s.jpg",
+            isPta: true,
+        },
+        a16: {
+            name: "Oppo A16",
+            brand: "Oppo",
+            colors: ["blue", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/oppo-a16.jpg",
+            isPta: true,
+        },
+        F9: {
+            name: "Oppo F9 (F9 Pro)",
+            brand: "Oppo",
+            colors: ["red", "orange"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/oppo-f9.jpg",
+            isPta: true,
+        },
+    },
+    // Infinix
+    infinix: {
+        note12pro: {
+            name: "Infinix Note 12 Pro 5G",
+            brand: "Infinix",
+            colors: ["black", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/infinix-note12-pro-5g.jpg",
+            isPta: true,
+        },
+        hot12play: {
+            name: "Infinix Hot 12 Play",
+            brand: "Infinix",
+            colors: ["gray", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/infinix-hot12-play.jpg",
+            isPta: true,
+        },
+        hot12: {
+            name: "Infinix Hot 12",
+            brand: "Infinix",
+            colors: ["blue", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/infinix-hot12.jpg",
+            isPta: true,
+        },
+        infinixnote12: {
+            name: "Infinix Note 12 Pro",
+            brand: "Infinix",
+            colors: ["blue", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/infinix-note12-pro-4g.jpg",
+            isPta: true,
+        },
+        infinixnote12: {
+            name: "Infinix Note 12",
+            brand: "Infinix",
+            colors: ["white", "orange"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/infinix-note12.jpg",
+            isPta: true,
+        },
+    },
+    // Xiaomi
+    Xiaomi: {
+        redminote11: {
+            name: "Xiaomi Redmi Note 11",
+            brand: "Redmi",
+            colors: ["lightblue", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-11-global.jpg",
+            isPta: true,
+        },
+        ultra12s: {
+            name: "Xiaomi 12S Ultra",
+            brand: "Redmi",
+            colors: ["black", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-12s-ultra.jpg",
+            isPta: true,
+        },
+        redminote10pro: {
+            name: "Xiaomi Redmi Note 10 Pro",
+            brand: "Redmi",
+            colors: ["bluewhite", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note10-pro.jpg",
+            isPta: true,
+        },
+        redminote11pro: {
+            name: "Xiaomi Redmi Note 11 Pro",
+            brand: "Redmi",
+            colors: ["white", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-11-pro-global.jpg",
+            isPta: true,
+        },
+        pocof4: {
+            name: "Xiaomi Poco F4",
+            brand: "Redmi",
+            colors: ["green", "White"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-poco-f4-5g-.jpg",
+            isPta: true,
+        },
+    },
+    // Huawei
+    huawei: {
+        matepadpro: {
+            name: "Huawei MatePad Pro 11 (2022)",
+            brand: "Huawei",
+            colors: ["black", "blue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/huawei-matepad-pro-11-2022-ofic.jpg",
+            isPta: true,
+        },
+        p50pro: {
+            name: "Huawei P50 Pro",
+            brand: "Huawei",
+            colors: ["gray", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/huawei-p50-pro.jpg",
+            isPta: true,
+        },
+        p9: {
+            name: "Huawei P9 Lite mini",
+            brand: "Huawei",
+            colors: ["gray", "orange"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/huawei-y6-pro-2017-p9-lite-mini.jpg",
+            isPta: true,
+        },
+        p30lite: {
+            name: "Huawei P30 Lite",
+            brand: "Huawei",
+            colors: ["white", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/huawei-p30-lite-.jpg",
+            isPta: true,
+        },
+        y9: {
+            name: "Huawei Y9 (2019)",
+            brand: "Huawei",
+            colors: ["blue", "Lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/huawei-y9-2019-.jpg",
+            isPta: true,
+        },
+    },
+    // Honor
+    Honor: {
+        x8: {
+            name: "Honor X8",
+            brand: "Huawei",
+            colors: ["gray", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/honor-x8.jpg",
+            isPta: true,
+        },
+        honor50: {
+            name: "Honor 50",
+            brand: "Huawei",
+            colors: ["orange", "red"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/honor-50-.jpg",
+            isPta: true,
+        },
+        honor8X: {
+            name: "Honor 8X",
+            brand: "Huawei",
+            colors: ["blue", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/huawei-honor-8x.jpg",
+            isPta: true,
+        },
+        x7: {
+            name: "Honor X7",
+            brand: "Huawei",
+            colors: ["blue", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/honor-x7.jpg",
+            isPta: true,
+        },
+        magic4pro: {
+            name: "Honor Magic4 Pro",
+            brand: "Huawei",
+            colors: ["white", "green"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/honor-magic4-pro.jpg",
+            isPta: true,
+        },
+    },
+    // ZTE
+    ZTE: {
+        nubiaRed: {
+            name: "ZTE nubia Red Magic 7S Pro",
+            brand: "ZTE",
+            colors: ["gray", "white"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/zte-nubia-red-magic-7s-pro.jpg",
+            isPta: true,
+        },
+        axon40: {
+            name: "ZTE Axon 40 Ultra",
+            brand: "ZTE",
+            colors: ["orange", "red"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/zte-axon-40-ultra.jpg",
+            isPta: true,
+        },
+        nubiaz40s: {
+            name: "ZTE nubia Z40S Pro",
+            brand: "ZTE",
+            colors: ["blue", "green"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/zte-nubia-z40s-pro.jpg",
+            isPta: true,
+        },
+        nubiaredmagic7pro: {
+            name: "ZTE nubia Red Magic 7 Pro",
+            brand: "ZTE",
+            colors: ["black", "lightblue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/zte-nubia-red-magic-7-pro.jpg",
+            isPta: true,
+        },
+        nubiaredmagic7: {
+            name: "ZTE nubia Red Magic 7",
+            brand: "ZTE",
+            colors: ["purple", "blue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/zte-nubia-red-magic-7.jpg",
+            isPta: true,
+        },
+    },
+    // Microsoft
+    Microsoft: {
+        surfaceduo2: {
+            name: "Microsoft Surface Duo 2",
+            brand: "Microsoft",
+            colors: ["black", "blue"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/microsoft-surface-duo2.jpg",
+            isPta: false,
+        },
+        surfaceduo: {
+            name: "Microsoft Surface duo",
+            brand: "Microsoft",
+            colors: ["white", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/microsoft-surface-duo.jpg",
+            isPta: false,
+        },
+        lumia650: {
+            name: "Microsoft lumia 650",
+            brand: "Microsoft",
+            colors: ["white", "gray"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/microsoft-lumia-650-s.jpg",
+            isPta: false,
+        },
+        lumia950: {
+            name: "Microsoft lumia 950 XL",
+            brand: "Microsoft",
+            colors: ["black", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/microsoft-lumia-950-xl--.jpg",
+            isPta: false,
+        },
+        lumia535: {
+            name: "Microsoft lumia 535",
+            brand: "Microsoft",
+            colors: ["black", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/nokia-lumia-535.jpg",
+            isPta: false,
+        },
+    },
+    // Google
+    Google: {
+        pixel6a: {
+            name: "Google Pixel 6a",
+            brand: "Google",
+            colors: ["green", "black"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/google-pixel-6a.jpg",
+            isPta: true,
+        },
+        pixel6pro: {
+            name: "Google Pixel 6 Pro",
+            brand: "Google",
+            colors: ["white", "wheet"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/google-pixel-6-pro.jpg",
+            isPta: true,
+        },
+        pixel6: {
+            name: "Google Pixel 6",
+            brand: "Google",
+            colors: ["orange", "red"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/google-pixel-6.jpg",
+            isPta: true,
+        },
+        pixel3: {
+            name: "Google Pixel 3",
+            brand: "Google",
+            colors: ["white", "wheet"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/google-pixel-3-.jpg",
+            isPta: true,
+        },
+        pixel3Xl: {
+            name: "Google Pixel 3 XL",
+            brand: "Google",
+            colors: ["white", "wheet"],
+            image:
+                "https://fdn2.gsmarena.com/vv/bigpic/google-pixel-3xl-.jpg",
+            isPta: true,
+        },
+    },
+}
+
+var list = Object.keys(mobilePhones);
+// console.log(list)
+for (var i = 0; i < list.length; i++) {
+    var dropdown = document.getElementById("dropdown");
+    var option = document.createElement("option");
+    var optionText = document.createTextNode(list[i]);
+    option.appendChild(optionText);
+    dropdown.appendChild(option);
+}
+
+
+function search() {
+    event.preventDefault()
+    var div12 = document.getElementById("div12");
+    var seletcedValue = dropdown.options[dropdown.selectedIndex].value;
+    console.log(seletcedValue)
+    var inputs = document.getElementById("input").value;
+    inputs = inputs.replaceAll(" ", '')
+    if (inputs in mobilePhones[seletcedValue]) {
+
+        // image
+        var img2 = document.createElement("img");
+        img2.setAttribute("src", mobilePhones[seletcedValue][inputs]["image"]);
+        img2.setAttribute("class", "qadeer");
+        div12.appendChild(img2);
+
+        // name
+        var h6 = document.createElement("h6");
+        var texth6 = document.createTextNode("Name: " + mobilePhones[seletcedValue][inputs]["name"]);
+        h6.appendChild(texth6);
+        h6.setAttribute("class", "qadeer");
+        div12.appendChild(h6);
+
+        // brand
+        var h62 = document.createElement("h6");
+        var texth62 = document.createTextNode("Brand: " + mobilePhones[seletcedValue][inputs]["brand"]);
+        h62.appendChild(texth62);
+        h62.setAttribute("class", "qadeer");
+        div12.appendChild(h62);
+
+        // colors
+        var h623 = document.createElement("h6");
+        var texth623 = document.createTextNode("Color: " + mobilePhones[seletcedValue][inputs]["colors"]);
+        h623.appendChild(texth623);
+        h623.setAttribute("class", "qadeer");
+        div12.appendChild(h623);
+
+        // ispta
+        var h6234 = document.createElement("h6");
+        var texth6234 = document.createTextNode("isPta: " + mobilePhones[seletcedValue][inputs]["isPta"]);
+        h6234.appendChild(texth6234);
+        h6234.setAttribute("class", "qadeer");
+        div12.appendChild(h6234);
+
+
+        var container1 = document.getElementById("none1");
+        container1.style.display = "block";
+
+        var container = document.getElementById("none");
+        container.style.display = "none";
+
+        // console.log(mobilePhones[seletcedValue][inputs])
+    }
+    else if (inputs == "") {
+        swal("pleaase select value first")
+    }
+    else {
+        swal("not found")
+    }
+}
+
+
